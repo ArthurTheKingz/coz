d535fdff37e0dbcd431518a355f296d73cd7c57e
Kng <104910669+ArthurTheKingz@users.noreply.github.com>
2023-05-28 17:26:23 -0300

    server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import os
 import openai
 from flask import Flask, request, jsonify
+import time
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
+
+last_request_time = 0
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -15,11 +18,19 @@ def hello():
 @app.route('/', methods=['POST'])
 def chat():
     try:
+        global last_request_time
         prompt = request.json['prompt']
         conversation_context = request.json.get('context')
-        
+
         if conversation_context:
             prompt = conversation_context + restart_sequence + prompt
+        
+        current_time = time.time()
+        time_elapsed = current_time - last_request_time
+
+        # Verifica se o tempo decorrido desde a última solicitação é menor que 2 segundos
+        if time_elapsed < 2:
+            time.sleep(2 - time_elapsed)
         
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -31,7 +42,9 @@ def chat():
             presence_penalty=0,
             stop=[" Human:", " AI:"]
         )
-        
+
+        last_request_time = time.time()
+
         choices = response.choices[0]
         conversation_context = choices['context'] if 'context' in choices else None
         bot_response = choices['text']
