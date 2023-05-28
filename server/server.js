e2e6abae5a08286c0c5325883e9bad36ba61999c
Kng <104910669+ArthurTheKingz@users.noreply.github.com>
2023-05-28 17:28:33 -0300

    server.js

--- server/server.js
+++ server/server.js
@@ -33,15 +33,15 @@ def chat():
             time.sleep(2 - time_elapsed)
         
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            temperature=0.9,
-            max_tokens=3000,
-            top_p=1,
-            frequency_penalty=0.5,
-            presence_penalty=0,
-            stop=[" Human:", " AI:"]
-        )
+  model="text-davinci-003",
+  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
 
         last_request_time = time.time()
 
